ed2fb7f4988429f6894ce271922f1097a2930fd6
david <davidalejo32@hotmail.com>
2022-06-15 20:23:31 -0500

    component TodoCounter styles and logic

--- src/components/TodoCounter/TodoCounter.jsx
+++ src/components/TodoCounter/TodoCounter.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './TodoCounter.scss';
 
 
-function TodoCounter (props) {
+function TodoCounter ( {totalTask, completedTask} ) {
   return(
     <div className="counter">
       <h2 className="counter__title"> <span>Task</span> Completed</h2>
-      <h2 className="counter__task"> 2 <span>/</span> 5 </h2>
+      <h2 className="counter__task"> {completedTask} <span>/</span> {totalTask} </h2>
     </div>
   );
 }
