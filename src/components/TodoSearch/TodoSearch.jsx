be5281483a86703ba45806ac38c8c867989d269c
david <davidalejo32@hotmail.com>
2022-06-15 20:22:36 -0500

    component TodoSearch styles and logic

--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './TodoSearch.scss';
 
-function TodoSearch() {
+function TodoSearch( {setSearchValue} ) {
+  
+  const inputText = (e) => {
+    setSearchValue(e.target.value)
+  }
+
   return(
     <div className="todoSeach">
-      <input className="todoSeach__input" placeholder="Search task..." type="text" />
+      <input 
+      className="todoSeach__input" 
+      placeholder="Search task..." 
+      type="text" 
+      onChange={inputText} 
+      
+      />
       <i className='bx bx-search todoSeach__icon'></i>
     </div>
   );
